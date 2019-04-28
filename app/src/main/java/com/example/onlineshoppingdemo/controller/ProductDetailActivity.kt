9042525aea7ef6c2b6072c3a192b46f94069bc49
Leon Soh <mokuyobikaji@gmail.com>
2019-04-28 18:28:39 +0800

    Add click listener for each product to navigate to product detail activity

--- app/src/main/java/com/example/onlineshoppingdemo/controller/ProductDetailActivity.kt
+++ app/src/main/java/com/example/onlineshoppingdemo/controller/ProductDetailActivity.kt
@@ -1,2 +1,21 @@
 package com.example.onlineshoppingdemo.controller
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.onlineshoppingdemo.R
+import com.example.onlineshoppingdemo.model.Product
+import com.example.onlineshoppingdemo.utilities.EXTRA_PRODUCT
+import kotlinx.android.synthetic.main.activity_product_detail.*
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+        val product = intent.getParcelableExtra<Product>(EXTRA_PRODUCT)
+        val resourceId = resources.getIdentifier(product.image, "drawable", packageName)
+        detailImageView.setImageResource(resourceId)
+        detailProductName.text = product.title
+        detailProductPrice.text = product.price
+    }
+}
